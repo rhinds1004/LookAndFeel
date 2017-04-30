533dd9584383b72fec1dd0d7b6b8315c5fff333f
rhinds1004 <rhinds1004@gmail.com>
2017-04-30 16:46:40 -0700

    Initial commit

---
+++ app/src/main/java/hindsr/uw/tacoma/edu/lookandfeel/StylesActivity.java
@@ -0,0 +1,13 @@
+package hindsr.uw.tacoma.edu.lookandfeel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StylesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_styles);
+    }
+}
