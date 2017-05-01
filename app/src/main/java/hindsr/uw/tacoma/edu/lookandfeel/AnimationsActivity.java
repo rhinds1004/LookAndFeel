51baa65b4e34492a10e581012d8c9b53eec4dba3
rhinds1004 <rhinds1004@gmail.com>
2017-04-30 22:00:32 -0700

    lab completed up to check point 2

--- app/src/main/java/hindsr/uw/tacoma/edu/lookandfeel/AnimationsActivity.java
+++ app/src/main/java/hindsr/uw/tacoma/edu/lookandfeel/AnimationsActivity.java
@@ -1,7 +1,12 @@
 package hindsr.uw.tacoma.edu.lookandfeel;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static android.animation.AnimatorInflater.loadAnimator;
 
 public class AnimationsActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class AnimationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animations);
+    }
+
+    public void animateSpin(View view){
+        AnimatorSet set = null;
+        View v = findViewById(R.id.animate_me);
+
+            set = (AnimatorSet) AnimatorInflater.loadAnimator(this,
+                    R.animator.rotate_and_spin);
+        set.setTarget(v);
+        set.start();
+    }
+
+    public void animateMove(View view){
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet)AnimatorInflater.loadAnimator(this, R.animator.move);
+        set.setTarget(v);
+        set.start();
+    }
+
+    public void animateFade(View view){
+        View v = findViewById(R.id.animate_me);
+        AnimatorSet set = (AnimatorSet)AnimatorInflater.loadAnimator(this, R.animator.fade);
+        set.setTarget(v);
+        set.start();
     }
 }
--- app/src/main/java/hindsr/uw/tacoma/edu/lookandfeel/StylesActivity.java
+++ app/src/main/java/hindsr/uw/tacoma/edu/lookandfeel/StylesActivity.java
@@ -1,7 +1,9 @@
 package hindsr.uw.tacoma.edu.lookandfeel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StylesActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class StylesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_styles);
+    }
+
+    /** Called when the user taps the Submit button */
+    public void submitButtonActivity(View view) {
+        Intent intent = new Intent(this, AnimationsActivity.class);
+
+        startActivity(intent);
     }
 }
